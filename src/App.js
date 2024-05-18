a4b036fb786f30f4ee05d685e6d6bdb4b8677af0
Student <student@d17-428-pc-19.ipa.ii.agh.edu.pl>
2024-05-18 14:09:52 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  let email = 'fracz@agh.edu.pl';
+  
+    const [email, setEmail] = useState('fracz@agh.edu.pl');
 
-  function handleChange(event) {
+    function handleChange(event) {
+        setEmail(event.target.value);
+    }
 
-    console.log(event.target.value)
+    let message = "Masz krótki e-mail"
+    
+    if(email.length>15){
+      message = "Masz dlugi e-mail"
+    }else if(email.length> 5){
+      message = "Masz ok e-mail"
+    }
 
-}
-  return (
-    <div className="App">
-      <h1>System do zapisów na zajęcia</h1>
-      <h2>Twój e-mail to: {email}</h2>
-      <input type="text" onChange={handleChange}/>
-    </div>
-  );
+    return (
+        <div>
+            <h1>System do zapisów na zajęcia</h1>
+            
+            <h2>Twój e-mail to {email}</h2>
+            <h3>{message}</h3>
+            <input type="text" value={email} onChange={handleChange}/>
+        </div>
+    );
+
 }
 
 export default App;
