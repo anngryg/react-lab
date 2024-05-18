f18a1254340d9c3c00de5e76e4fd28ebfa2ba076
Student <student@d17-428-pc-19.ipa.ii.agh.edu.pl>
2024-05-18 14:53:06 +0200

    third commit

--- src/App.js
+++ src/App.js
@@ -3,10 +3,18 @@ import { useState } from 'react';
 
 function App() {
   
-    const [email, setEmail] = useState('fracz@agh.edu.pl');
-
+    const [email, setEmail] = useState('');
+    const [isLogggedIn, setIsLoggedIn] = useState(false);
     function handleChange(event) {
         setEmail(event.target.value);
+    }
+
+    function handleClick(){
+      if (isLogggedIn==true){
+        setIsLoggedIn(false)
+      }else{
+        setIsLoggedIn(true)
+      }
     }
 
     let message = "Masz krótki e-mail"
@@ -16,14 +24,25 @@ function App() {
     }else if(email.length> 5){
       message = "Masz ok e-mail"
     }
+    let content;
+    if(isLogggedIn==true){
+      content = <div>
+        <h2>Witaj, {email}</h2>
+        <button onClick={handleClick}>Wyloguj</button> 
+      </div>
+    }else{
+      content = <div>
+        <h2>Twój e-mail to {email}</h2>
+        <h3>{message}</h3>
+        <input type="text" value={email} onChange={handleChange}/>
+        <button onClick={handleClick}>Wchodzę</button> 
+      </div>
+    }
 
     return (
         <div>
             <h1>System do zapisów na zajęcia</h1>
-            
-            <h2>Twój e-mail to {email}</h2>
-            <h3>{message}</h3>
-            <input type="text" value={email} onChange={handleChange}/>
+            {content}
         </div>
     );
 
